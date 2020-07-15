c6215fec69a0280ad9382696ba1e4fcb117244f1
Zachary Sciscente <zsciscente@gmail.com>
2020-07-15 19:28:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,11 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(color)
-  puts "#{color[0]}: #{color}"
+  puts "R:#{color[0]}"
+  puts "O:#{color[1]}"
+  puts "Y:#{color[2]}"
+  puts "G:#{color[3]}"
+  puts "B:#{color[4]}"
+  puts "I:#{color[5]}"
+  puts "V:#{color[6]}"
 end
